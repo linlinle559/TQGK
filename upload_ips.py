126cb299b1ba9b765362ea4d5226dee42c8d248f
linlinle559 <linlinle559@gmail.com>
2025-02-24 17:52:33 +0800

    Update upload_ips.py

--- upload_ips.py
+++ upload_ips.py
@@ -11,7 +11,7 @@ commit_message = "Update bestcf IP list"
 
 # API 地址
 csv_url = "https://ipdb.030101.xyz/api/bestcf.csv"
-custom_suffix = "可变"  # 可自定义后缀
+custom_suffix = "优选IP"  # 可自定义后缀
 
 def download_csv(url):
     response = requests.get(url)
